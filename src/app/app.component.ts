f499678865f7f80960b4b514818e31b860884124
vladleontescu <vladleontescu@coherentsolutions.com>
2022-05-31 20:01:03 +0300

    completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = '003-todo-angular';
+    title = 'ToDoApp';
+
+    filter: 'all' | 'active' | 'done' = 'all';
+
+    allItems = [
+        { description: 'code', done: true },
+        { description: 'eat', done: false },
+        { description: 'sleep', done: false },
+        { description: 'repeat', done: false },
+    ];
+
+    get items() {
+        if (this.filter === 'all') {
+            return this.allItems;
+        }
+        return this.allItems.filter((item) => (this.filter === 'done' ? item.done : !item.done));
+    }
+
+    addItem(description: string) {
+        this.allItems.unshift({
+            description,
+            done: false,
+        });
+    }
+
+    remove(item: any) {
+        this.allItems.splice(this.allItems.indexOf(item), 1);
+    }
 }
